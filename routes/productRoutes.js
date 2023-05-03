1db41390c484cf68b407031611a3834b74bd5558
dmuthui <vyda2002us@gmail.com>
2023-05-04 00:43:12 +0300

    Adds changes to Server

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -14,48 +14,4 @@ router.get('/', (req,res)=>{
    })
 })
 
-// @route POST /products
-router.post('/', (req,res)=>{
-   console.log(req.body)
-   // Create a product item
-   const newProduct = new Product({
-       name: req.body.name,
-       description: req.body.description,
-       price: req.body.price,
-       quantity: req.body.quantity,
-   });
-
-   newProduct.save((err, product)=>{
-       if (err) console.log(err)
-       res.json(product)
-   })
-})
-
-// @desc  Update a product
-router.put('/:id', (req,res)=>{
-   // Update a product in the database
-   Product.updateOne({_id:req.params.id},{
-       name: req.body.name,
-       description: req.body.description,
-       price: req.body.price,
-       quantity: req.body.quantity,
-   }, {upsert: true}, (err, product)=>{
-       if(err) console.log(err);
-       res.json(product)
-   })
-})
-
-// @desc  Delete a product
-router.delete('/:id', (req,res)=>{
-   // Delete a product from database
-   Product.deleteOne({_id: req.params.id}, (err)=>{
-       if (err){
-           console.log(err)
-           res.json({success:false})
-       }else{
-           res.json({success:true})
-       }
-   })
-})
-
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
+//Initialising imports
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
-
 const productRoute = require('./routes/productRoutes');
-const uploadFiles = require('./routes/uploadFiles');
+const Product = require('./models/product');
+
 
 // Initializing express
 const app = express()
@@ -15,50 +15,32 @@ app.use(cors())
 
 
 //DB config
-const MONGODB_URI= process.env.MONGODB_URI ||
-
-require('./config').mongoDB_URI
+const MONGODB_URI = process.env.MONGODB_URI ||
+   require('./config').mongoDB_URI
 
 // Connect to MongoDB
-
-mongoose.connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true  })
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 
 // Check Connection
 let db = mongoose.connection;
-
-db.once('open', ()=>{
-
-   console.log('Database connected successfully')
-
+db.once('open', () => {
+   console.log('Database connected successfully!')
 })
 
 // Check for DB Errors
-
-db.on('error', (error)=>{
-
+db.on('error', (error) => {
    console.log(error);
+   mongoose.set('StrictQuery', true);
+})
 
-mongoose.set('StrictQuery', true);
-   // Use Routes
- })
- app.use('/product', productRoute)  //new code
- app.use(uploadFiles);
- 
-//  // Serve our static assets if in production
-if(process.env.NODE_ENV === 'production'){
-   // set a static folder
-   app.use(express.static('client/build'));
+// Use Routes
+app.use('/Product', productRoute)
 
-   app.get('*', (req,res)=>{
-       res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-   })
-
-}
- 
 // Define the PORT
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, ()=>{
+//Listening to port
+app.listen(PORT, () => {
    console.log(`Server listening on port ${PORT}`)
 })
